9038b5ba541437f4f45fe3da43e8e4fbcfaf8ead
omprakash <149952504+PYTH367@users.noreply.github.com>
2026-06-20 21:57:37 +0530

    Change SECRET_KEY in app.py
    Updated the secret key for the application.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 # ⚠️ Change this to your own secret password!
-SECRET_KEY = "chiku123"  # change this
+SECRET_KEY = "chikumeow"  # change this
 
 VISITORS_FILE = "visitors.json"
 
